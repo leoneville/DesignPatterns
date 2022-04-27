ea4afe839fa61dfe6df43f9f1699d19963eff20f
leoneville <leoneville_@hotmail.com>
2022-04-27 17:12:00 -0300

    practicing Singleton Single Thread

---
+++ ContaCorrenteSingl/ContaCorrenteSingl/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrenteSingl
+{
+    public class ContaCorrente
+    {
+        public string NumeroConta { get; set; }
+        private static ContaCorrente instance;
+
+        private ContaCorrente()
+        {
+
+        }
+
+        public static ContaCorrente Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ContaCorrente();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ ContaCorrenteSingl/ContaCorrenteSingl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ContaCorrenteSingl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            criarContas();
+        }
+
+        public static void criarContas()
+        {
+            ContaCorrente ct1 = ContaCorrente.Instance;
+            ct1.NumeroConta = "123-45";
+            Console.WriteLine($"{ct1} - {ct1.GetHashCode()}");
+            ContaCorrente ct2 = ContaCorrente.Instance;
+            ct2.NumeroConta = "323-35";
+            Console.WriteLine($"{ct2} - {ct2.GetHashCode()}");
+        }
+    }
+}
