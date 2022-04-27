3d08052b0dfe5742dd1791507d752ce87d081b79
leoneville <leoneville_@hotmail.com>
2022-04-27 15:48:36 -0300

    learning Singleton Single Thread

---
+++ Singleton/Singleton_SingleThread/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private int numeroDeInstancias = 0;
+
+        private Singleton()
+        {
+            Console.WriteLine("Instanciando dentro do construtor privado");
+            numeroDeInstancias++;
+            Console.WriteLine($"Número de Instancias = {numeroDeInstancias}");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Console.WriteLine("Criando a primeira instancia!");
+                    instance = new Singleton();
+                }
+                
+                return instance;
+            }
+        }
+    }
+}
