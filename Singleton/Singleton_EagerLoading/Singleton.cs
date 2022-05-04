1e8bfb308fc3b73367a6cd37c6677d31ee551465
leoneville <leoneville_@hotmail.com>
2022-05-04 08:33:20 -0300

    learning Singleton Eager Loading and Lazy Loading

---
+++ Singleton/Singleton_EagerLoading/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_EagerLoading
+{
+    public sealed class Singleton
+    {
+        // eager loading
+        private static readonly Singleton instance = new Singleton();
+        private static int contador = 0;
+
+        private Singleton()
+        {
+            contador++;
+            WriteLine($"Instancia: {contador}");
+        }
+        
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
