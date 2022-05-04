6051de2740003f7d5ffb2b258d6b859461850459
leoneville <leoneville_@hotmail.com>
2022-05-04 08:10:49 -0300

    learning Singleton Static and Sealed

---
+++ Singleton/Singleton_Estatico/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton_Estatico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => AcessoThread1(),
+                () => AcessoThread2(),
+                () => AcessoThread3()
+            );
+        }
+
+        private static void AcessoThread1()
+        {
+            Singleton s1 = Singleton.Instance;
+            Console.WriteLine("Thread 1");
+        }
+
+        private static void AcessoThread2()
+        {
+            Singleton s2 = Singleton.Instance;
+            Console.WriteLine("Thread 2");
+        }
+
+        private static void AcessoThread3()
+        {
+            Singleton s3 = Singleton.Instance;
+            Console.WriteLine("Thread 3");
+        }
+    }
+}
---
+++ Singleton/Singleton_Estatico/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Estatico
+{
+    public sealed class Singleton
+    {
+        private static int contador = 0;
+        private static readonly Singleton instance;
+
+        private Singleton() { }
+        
+        static Singleton()
+        {
+            contador++;
+            WriteLine($"Instância: {contador}");
+            instance = new Singleton();
+        }
+        
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
