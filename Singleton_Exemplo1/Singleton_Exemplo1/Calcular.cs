0cf4a31308e66dc603fe4351884d568edc203f0a
leoneville <leoneville_@hotmail.com>
2022-05-04 09:02:13 -0300

    practicing Singleton Pattern

---
+++ Singleton_Exemplo1/Singleton_Exemplo1/Calcular.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Exemplo1
+{
+    public sealed class Calcular
+    {
+        private static Calcular instance;
+        private static readonly object padlock = new object();
+
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set; }
+
+        private Calcular() { }
+
+        public static Calcular Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Calcular();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public double Somar()
+        {
+            return Valor1 + Valor2;
+        }
+
+        public double Subtrair()
+        {
+            return Valor1 - Valor2;
+        }
+
+        public double Multiplicar()
+        {
+            return Valor1 * Valor2;
+        }
+
+        public double Dividir()
+        {
+            return Valor1 / Valor2;
+        }
+    }
+}
---
+++ Singleton_Exemplo1/Singleton_Exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace Singleton_Exemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v1 = Calcular.Instance.Valor1 = 10.5;
+            var v2 = Calcular.Instance.Valor2 = 5.5;
+
+            WriteLine($"Valor1 = {v1} Valor2 = {v2}");
+
+            WriteLine("\n----------------------\n");
+
+            WriteLine("Adição: " + Calcular.Instance.Somar());
+            WriteLine("Subtração: " + Calcular.Instance.Subtrair());
+            WriteLine("Multiplicação: " + Calcular.Instance.Multiplicar());
+            WriteLine("Divisão: " + Calcular.Instance.Dividir());
+
+            WriteLine("\n----------------------\n");
+        }
+    }
+}
