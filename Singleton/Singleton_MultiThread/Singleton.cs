5952fcacabafc12edee57fb285270a64c7ba65bb
leoneville <leoneville_@hotmail.com>
2022-05-03 21:27:15 -0300

    learning Singleton Thread-Safety with double checking

---
+++ Singleton/Singleton_MultiThread/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_ThreadSafety
+{
+    public class Singleton
+    {
+        private static int contador = 0;
+        private static Singleton instance;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+            contador++;
+            WriteLine($"Instancia {contador}");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
